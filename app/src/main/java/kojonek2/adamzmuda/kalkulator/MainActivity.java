dfef21f4d568807c0e0fad59b7b47523278d77a8
kojonek2 <kojonek2@wp.pl>
2016-10-02 09:28:43 +0200

    Update keyboard

--- app/src/main/java/kojonek2/adamzmuda/kalkulator/MainActivity.java
+++ app/src/main/java/kojonek2/adamzmuda/kalkulator/MainActivity.java
@@ -2,6 +2,7 @@ package kojonek2.adamzmuda.kalkulator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +11,38 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    /////////////Start Digits///////////////////
+
+    public void displayNumber0(View view) {
+    }
+
+    public void displayNumber1(View view) {
+    }
+
+    public void displayNumber2(View view) {
+    }
+
+    public void displayNumber3(View view) {
+    }
+
+    public void displayNumber4(View view) {
+    }
+
+    public void displayNumber5(View view) {
+    }
+
+    public void displayNumber6(View view) {
+    }
+
+    public void displayNumber7(View view) {
+    }
+
+    public void displayNumber8(View view) {
+    }
+
+    public void displayNumber9(View view) {
+    }
+
+    ////////////////End Digits///////////////
 }
