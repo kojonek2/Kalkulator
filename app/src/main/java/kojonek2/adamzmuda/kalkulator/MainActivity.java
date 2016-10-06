9b356db625603e2b4d2704b6f4656d3329a593cc
kojonek2 <kojonek2@wp.pl>
2016-10-06 20:55:44 +0200

    Added toast when max number of digits

--- app/src/main/java/kojonek2/adamzmuda/kalkulator/MainActivity.java
+++ app/src/main/java/kojonek2/adamzmuda/kalkulator/MainActivity.java
@@ -4,14 +4,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    //constant
     static final String STATE_NUMBER_LENGTH = "numberOnDisplayLength";
     static final String STATE_NUMBER = "numberOnDisplay";
+    static final String STATE_FIRST_INPUT = "firstNumberInInput";
+    static final String STATE_MATH_OPERATOR = "mathOperatorChossen";
 
+    //variables
     TextView textView;
     int numbersOnDisplay;
+    double firstInput;
+    String mathOperatorInput;
+    Toast toast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,12 +27,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         textView = (TextView) findViewById(R.id.display1);
+        toast = Toast.makeText(this, R.string.toastMaxDigits, Toast.LENGTH_SHORT);
 
         //reading information
         if (savedInstanceState != null) {
+
             numbersOnDisplay = savedInstanceState.getInt(STATE_NUMBER_LENGTH);
             textView.setText(savedInstanceState.getCharSequence(STATE_NUMBER));
+            firstInput = savedInstanceState.getDouble(STATE_FIRST_INPUT);
+            mathOperatorInput = savedInstanceState.getString(STATE_MATH_OPERATOR);
         } else {
+
+            //default ones
             numbersOnDisplay = 0;
         }
     }
@@ -36,72 +50,94 @@ public class MainActivity extends AppCompatActivity {
         //saving information
         savedInstanceState.putInt(STATE_NUMBER_LENGTH, numbersOnDisplay);
         savedInstanceState.putCharSequence(STATE_NUMBER, textView.getText());
+        savedInstanceState.putDouble(STATE_FIRST_INPUT, firstInput);
+        savedInstanceState.putString(STATE_MATH_OPERATOR, mathOperatorInput);
     }
 
     public void displayNumber(View view) {
-        switch (view.getId()){
+        switch (view.getId()) {
             case R.id.button_0:
                 if (numbersOnDisplay < 15) {
                     textView.setText(textView.getText().equals("0") ? "0" : textView.getText() + "0");
                     numbersOnDisplay++;
+                } else {
+                    toast.show();
                 }
                 break;
             case R.id.button_1:
                 if (numbersOnDisplay < 15) {
                     textView.setText(textView.getText().equals("0") ? "1" : textView.getText() + "1");
                     numbersOnDisplay++;
+                } else {
+                    toast.show();
                 }
                 break;
             case R.id.button_2:
                 if (numbersOnDisplay < 15) {
                     textView.setText(textView.getText().equals("0") ? "2" : textView.getText() + "2");
                     numbersOnDisplay++;
+                } else {
+                    toast.show();
                 }
                 break;
             case R.id.button_3:
                 if (numbersOnDisplay < 15) {
                     textView.setText(textView.getText().equals("0") ? "3" : textView.getText() + "3");
                     numbersOnDisplay++;
+                } else {
+                    toast.show();
                 }
                 break;
             case R.id.button_4:
                 if (numbersOnDisplay < 15) {
                     textView.setText(textView.getText().equals("0") ? "4" : textView.getText() + "4");
                     numbersOnDisplay++;
+                } else {
+                    toast.show();
                 }
                 break;
             case R.id.button_5:
                 if (numbersOnDisplay < 15) {
                     textView.setText(textView.getText().equals("0") ? "5" : textView.getText() + "5");
                     numbersOnDisplay++;
+                } else {
+                    toast.show();
                 }
                 break;
             case R.id.button_6:
                 if (numbersOnDisplay < 15) {
                     textView.setText(textView.getText().equals("0") ? "6" : textView.getText() + "6");
                     numbersOnDisplay++;
+                } else {
+                    toast.show();
                 }
                 break;
             case R.id.button_7:
                 if (numbersOnDisplay < 15) {
                     textView.setText(textView.getText().equals("0") ? "7" : textView.getText() + "7");
                     numbersOnDisplay++;
+                } else {
+                    toast.show();
                 }
                 break;
             case R.id.button_8:
                 if (numbersOnDisplay < 15) {
                     textView.setText(textView.getText().equals("0") ? "8" : textView.getText() + "8");
                     numbersOnDisplay++;
+                } else {
+                    toast.show();
                 }
                 break;
             case R.id.button_9:
                 if (numbersOnDisplay < 15) {
                     textView.setText(textView.getText().equals("0") ? "9" : textView.getText() + "9");
                     numbersOnDisplay++;
+                } else {
+                    toast.show();
                 }
                 break;
             case R.id.button_backspace:
-                textView.setText(textView.getText().length() > 1 &&(textView.getText().length() > 2 || !textView.getText().toString().substring(0, 1).equals("-")) ? textView.getText().toString().substring(0, textView.getText().length() - 1) : "0");
+                textView.setText(textView.getText().length() > 1 && (textView.getText().length() > 2 || !textView.getText().toString().substring(0, 1).equals("-")) ? textView.getText().toString().substring(0, textView.getText().length() - 1) : "0");
                 numbersOnDisplay--;
                 break;
             case R.id.button_c:
